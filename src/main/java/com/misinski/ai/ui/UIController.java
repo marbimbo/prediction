40367c66daa706314c27785411c44f94116099b7
Marcin Misinski <marbimbo@gmail.com>
2018-03-14 17:22:28 +0100

    Add ChoiceBox for prediction

--- src/main/java/com/misinski/ai/ui/UIController.java
+++ src/main/java/com/misinski/ai/ui/UIController.java
@@ -1,9 +1,12 @@
 package com.misinski.ai.ui;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Separator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,12 +14,20 @@ import java.util.ResourceBundle;
 public class UIController implements Initializable {
 
     @FXML
+    private ChoiceBox prediction_choice;
+
+    @FXML
     private LineChart exchange_chart;
 
     @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        prediction_choice.setItems(FXCollections.observableArrayList(
+                "Brak",
+                new Separator(),
+                "tydzień", "miesiąc", "rok")
+        );
+        prediction_choice.setValue("Brak");
     }
 
     public void addSeries(XYChart.Series series) {
